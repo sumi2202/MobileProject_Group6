84a4a6c1716c6fe82db21a8dffb81017423a4eff
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-11-25 15:44:52 -0500

    first empty commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FoodDelivery_MobileProject_Group6"
+include(":app")
