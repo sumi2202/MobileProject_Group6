fa22380c84ca08ddb21bf4e1c2196df21e239a66
Laksumi Soma <laksumi2202@gmail.com>
2023-11-25 15:47:51 -0500

    Main page java files

---
+++ app/src/main/java/com/example/fooddelivery_mobileproject_group6/MainPage_Activity.java
@@ -0,0 +1,42 @@
+package com.example.project_mainpage;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainPage_Activity extends AppCompatActivity {
+
+    // creating object of ViewPager
+    ViewPager mViewPager;
+
+    // images array
+    int[] images = {R.drawable.pizza1, R.drawable.chicken_wings1, R.drawable.thai_curry1, R.drawable.burgers_fries1,
+            R.drawable.burritos1, R.drawable.biryani1, R.drawable.sushi1, R.drawable.shawarma1};
+
+    // Creating Object of ViewPagerAdapter
+    ViewPagerAdapter mViewPagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            /*Cosmetic purpose; It sets the task bar and status bar with the same colour as the colour
+            theme I chose to work with*/
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.carmine));
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.carmine));
+        }
+
+        // Initializing the ViewPager Object
+        mViewPager = (ViewPager)findViewById(R.id.restaurantCarousel);
+
+        // Initializing the ViewPagerAdapter
+        mViewPagerAdapter = new ViewPagerAdapter(MainPage_Activity.this, images);
+
+        // Adding the Adapter to the ViewPager
+        mViewPager.setAdapter(mViewPagerAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/fooddelivery_mobileproject_group6/ViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.project_mainpage;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import java.util.Objects;
+
+class ViewPagerAdapter extends PagerAdapter {
+    Context context;
+    int[] images;
+    LayoutInflater mLayoutInflater;
+    public ViewPagerAdapter(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.image_viewer, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageViewMain);
+
+        imageView.setImageResource(images[position]);
+
+        Objects.requireNonNull(container).addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        container.removeView((LinearLayout) object);
+    }
+}
