7f732b8d887f1e7d3eb2ccec2cf886f507df1efd
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-12-04 23:42:06 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/fooddelivery_mobileproject_group6/MainActivity.java
+++ app/src/main/java/com/example/fooddelivery_mobileproject_group6/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.fooddelivery_mobileproject_group6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            /*Cosmetic purpose; It sets the task bar and status bar with the same colour as the colour
+            theme I chose to work with*/
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.carmine));
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.carmine));
+        }
 
         db = new DBHandler(this);           //Creates DBHandler object
         emailEDT = (EditText) findViewById(R.id.lemailET);      //Finds email edittext by id
